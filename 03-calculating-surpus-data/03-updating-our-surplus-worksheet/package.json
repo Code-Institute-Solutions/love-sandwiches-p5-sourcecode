277070b51b82ce1eef4a80d59ab503dd76460c6f
Anna Greaves <gilhespy.anna@gmail.com>
2021-03-25 12:11:03 +0100

    Add source code for updating surplus worksheet

---
+++ 03-calculating-surpus-data/03-updating-our-surplus-worksheet/package.json
@@ -0,0 +1,11 @@
+{
+    "postinstall": "cd ci-xterm && npm install",
+    "dependencies": {
+      "node": "^15.8.0",
+      "node-static": "^0.7.11",
+      "python-shell": "^2.0.3",
+      "socket.io": "^3.1.2",
+      "xterm": "^4.10.0"
+    }
+  }
+  
--- 03-calculating-surpus-data/03-updating-our-surplus-worksheet/run.py
+++ 03-calculating-surpus-data/03-updating-our-surplus-worksheet/run.py
@@ -66,16 +66,6 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
-
-
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -103,7 +93,7 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    print(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
