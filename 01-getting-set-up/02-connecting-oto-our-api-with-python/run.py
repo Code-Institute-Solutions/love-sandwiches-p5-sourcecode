54f73a1a2d9922b9e2253380eb3d18ea1d9a7ac5
Anna Greaves <gilhespy.anna@gmail.com>
2021-03-25 10:53:31 +0100

    Add source code for connecting to API video

---
+++ 01-getting-set-up/02-connecting-oto-our-api-with-python/run.py
@@ -0,0 +1,19 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
