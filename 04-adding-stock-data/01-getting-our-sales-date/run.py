b27ab99e17cc423f75e7e0235761a65a387eb34f
NielMc <niel@codeinstitute.net>
2021-10-26 10:00:01 +0100

    Added function to get last 5 entries

--- 04-adding-stock-data/01-getting-our-sales-date/run.py
+++ 04-adding-stock-data/01-getting-our-sales-date/run.py
@@ -87,6 +87,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
